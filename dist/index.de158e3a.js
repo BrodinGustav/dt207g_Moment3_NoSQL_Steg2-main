6e44a7726fcd0ffa218386afa055fcef534f94c0
Gustav Brodin <brodin.gustav@gmail.com>
2024-04-25 11:21:13 +0200

    Justerat main.js för att få utskrift. Serer skickar JSON data men får error på fronten att JSON data inte kan läsas. Ändrar om så Backticks används. Men får nu "unexptected null" vid npm run start

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,16 +1,22 @@
 //Formulär för att hämta data från servern
 async function fetchCvs() {
-    const respone = await fetch("/cv");
-    const data = await respone.json();
-    const cvListElement = document.getElementById("cv-list");
-    cvListElement.innerHTML = "";
-    data.forEach((cv)=>{
-        const cvElement = document.createElement("div");
-        cvElement.innerHTML = "<p><strong>F\xf6retagsnamn:</strong>${cv.companyname}</p>, <p><strong>Jobttitel:</strong>${cv.jobtitle}</p>, <p><strong>Arbetsplats:</strong>${cv.location}</p>";
-        cvListElement.appendChild(cvElement);
-    });
+    try {
+        const respone = await fetch("/cv");
+        const data = await respone.json();
+        const cvListElement = document.getElementById("cv-list");
+        cvListElement.innerHTML = "";
+        data.forEach((cv)=>{
+            const cvElement = document.createElement("div");
+            cvElement.innerHTML = `<p><strong>F\xf6retagsnamn:</strong>${cv.companyname}</p>
+                        <p><strong>Jobttitel:</strong>${cv.jobtitle}</p>
+                        <p><strong>Arbetsplats:</strong>${cv.location}</p>`;
+            cvListElement.appendChild(cvElement);
+        });
+    } catch (error) {
+        console.error("Fel uppstod vid h\xe4mtning av CV-data:", error);
+    }
 }
 // Ladda in CV:er när sidan laddas
-fetchCVs();
+fetchCvs();
 
 //# sourceMappingURL=index.de158e3a.js.map
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //Formulär för att hämta data från servern
 async function fetchCvs(){
+  try{
   const respone = await fetch("/cv");
   const data = await respone.json();
   const cvListElement = document.getElementById("cv-list");
@@ -9,10 +10,14 @@ cvListElement.innerHTML = "";
 
 data.forEach(cv => {
   const cvElement = document.createElement("div");
-  cvElement.innerHTML ='<p><strong>Företagsnamn:</strong>${cv.companyname}</p>, <p><strong>Jobttitel:</strong>${cv.jobtitle}</p>, <p><strong>Arbetsplats:</strong>${cv.location}</p>';
+  cvElement.innerHTML =`<p><strong>Företagsnamn:</strong>${cv.companyname}</p> <p><strong>Jobttitel:</strong>${cv.jobtitle}</p>, <p><strong>Arbetsplats:</strong>${cv.location}</p>`;
 
   cvListElement.appendChild(cvElement);
 });
+} catch (error){
+  console.error("Fel uppstod vid hämtning av CV-data:", error);
 }
+}
+
   // Ladda in CV:er när sidan laddas
-  fetchCVs();
+  fetchCvs();
